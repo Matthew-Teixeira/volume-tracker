629fe815201369e9b8646fdf5e63f89a809c11b0
ApolloSolo <matt1teixeira@gmail.com>
2022-07-16 10:22:45 -0400

    Volume routes and controls

--- server/controllers/volumeControllers.js
+++ server/controllers/volumeControllers.js
@@ -1,19 +1,42 @@
 const { Tank, Volume } = require("../models/index");
 const asyncHandler = require("express-async-handler");
 
+const getVolume = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const volume = await Volume.findById(id);
+  return res.status(200).json(volume);
+});
+
 const addVolume = asyncHandler(async (req, res) => {
   const { tankName } = req.params;
-  console.log(tankName)
+  console.log(tankName);
   const newVolume = await Volume.create(req.body);
   const tank = await Tank.findOneAndUpdate(
     { tankName },
     { $push: { volumes: newVolume._id } },
     { new: true }
-  ).populate("volumes").select("-__v");
+  )
+    .populate("volumes")
+    .select("-__v");
 
-  return res.status(200).json(tank)
+  return res.status(200).json(tank);
+});
+
+const updateVolume = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const volume = await Volume.findByIdAndUpdate(id, req.body, { new: true });
+  return res.status(200).json(volume);
+});
+
+const deleteVolume = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const volume = await Volume.findByIdAndDelete(id);
+  return res.status(200).json(volume);
 });
 
 module.exports = {
-    addVolume
-}
+  getVolume,
+  addVolume,
+  updateVolume,
+  deleteVolume,
+};
--- server/routes/api/volumeRoutes.js
+++ server/routes/api/volumeRoutes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
-const { addVolume } = require("../../controllers/volumeControllers");
+const { getVolume, addVolume, updateVolume, deleteVolume } = require("../../controllers/volumeControllers");
 
+router.get("/:id", getVolume);
 router.post("/addVolume/:tankName", addVolume);
+router.put("/:id", updateVolume);
+router.delete("/:id", deleteVolume);
 
 module.exports = router;
