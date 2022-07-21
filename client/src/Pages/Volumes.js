95338fb9127409ee85c7b44bb67c3de47c19b889
ApolloSolo <matt1teixeira@gmail.com>
2022-07-21 19:05:07 -0400

    Pagination

--- client/src/Pages/Volumes.js
+++ client/src/Pages/Volumes.js
@@ -1,23 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import VolumeTable from "../components/VolumeTable";
+import ReactPaginate from "react-paginate";
 import { useNavigate } from "react-router-dom";
 import Auth from "../utils/auth";
+
+import loader from "../assets/images/loader.gif";
 
 const Volumes = () => {
   const [tankId, setTankId] = useState("");
   const [tankData, setTankData] = useState({});
   const [loading, setLoading] = useState(true);
+  const [loading2, setLoading2] = useState(true);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const userLoggedIn = Auth.loggedIn();
   const userToken = Auth.getToken();
 
   useEffect(() => {
     if (!userLoggedIn) {
-      navigate('/login')
+      navigate("/login");
     }
-  }, [userLoggedIn, navigate])
+  }, [userLoggedIn, navigate]);
 
   const getTankId = () => {
     let path = window.location.pathname.split("/");
@@ -39,7 +43,6 @@ const Volumes = () => {
       })
         .then((response) => response.json())
         .then((data) => {
-          console.log("Success:", data);
           setTankData(data);
           setLoading(false);
         })
@@ -50,40 +53,86 @@ const Volumes = () => {
     if (tankId) {
       getTankData(tankId);
     }
-  }, [tankId]);
+  }, [tankId, userToken]);
+
+  // We start with an empty list of items.
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+  // Here we use item offsets; we could also use page offsets
+  // following the API or data you're working with.
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 10;
+
+  useEffect(() => {
+    // Fetch items from another resources.
+    if (tankData.volumes) {
+      const endOffset = itemOffset + itemsPerPage;
+      setCurrentItems(tankData.volumes.slice(itemOffset, endOffset));
+      setPageCount(Math.ceil(tankData.volumes.length / itemsPerPage));
+      setLoading2(false);
+    }
+  }, [itemOffset, itemsPerPage, tankData]);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % tankData.volumes.length;
+    setItemOffset(newOffset);
+  };
 
   return (
     <div className="min-h-[calc(100vh-64px)] h-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
-        <Link to={`/addVolume/${tankData._id}`}>
-            <button className="w-full block mx-auto max-w-[300px] rounded-md bg-[#337499]  py-2 text-indigo-100 hover:bg-[#4f90b6] hover:shadow-lg duration-75 text-lg my-4">
-              Add Volume
-            </button>
-          </Link>
-      {loading ? (
-        <div className="text-4xl text-center mt-4">Loading...</div>
+      <Link to={`/addVolume/${tankData._id}`}>
+        <button className="w-full block mx-auto max-w-[300px] rounded-md bg-[#337499]  py-2 text-indigo-100 hover:bg-[#4f90b6] hover:shadow-lg duration-75 text-lg my-4">
+          Add Volume
+        </button>
+      </Link>
+      {loading || loading2 ? (
+        <div className="mt-24 flex justify-center items-center">
+          <img alt="loading" src={loader} />
+        </div>
       ) : (
-        <div className="overflow-x-auto container flex md:justify-center mx-auto">
-          <div className="flex flex-col">
+        <>
+          <div className="overflow-x-auto container flex md:justify-center mx-auto">
+            <div className="flex flex-col">
               <div className="border-b border-gray-200 shadow">
-              <h3 className='text-center text-xl font-bold'>{tankData.tankName.slice(0, 1) + tankData.tankName[1].toUpperCase()}</h3>
-    <table className="divide-y divide-gray-300 ">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th className="px-6 py-2 text-xs text-gray-500">Date</th>
-                    <th className="px-6 py-2 text-xs text-gray-500">Time</th>
-                    <th className="px-6 py-2 text-xs text-gray-500">Total</th>
-                    <th className="px-6 py-2 text-xs text-gray-500">PCW</th>
-                    <th className="px-6 py-2 text-xs text-gray-500">LNAPL</th>
-                    <th className="px-6 py-2 text-xs text-gray-500">Edit</th>
-                  </tr>
-                </thead>
-                {tankData.volumes.map((volume) => (
-                  <VolumeTable key={volume._id} volumeData={volume} tankData={tankData} />
-                ))}
+                <h3 className="text-center text-xl font-bold">
+                  {tankData.tankName.slice(0, 1) +
+                    tankData.tankName[1].toUpperCase()}
+                </h3>
+                <table className="divide-y divide-gray-300 ">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th className="px-6 py-2 text-xs text-gray-500">Date</th>
+                      <th className="px-6 py-2 text-xs text-gray-500">Time</th>
+                      <th className="px-6 py-2 text-xs text-gray-500">Total</th>
+                      <th className="px-6 py-2 text-xs text-gray-500">PCW</th>
+                      <th className="px-6 py-2 text-xs text-gray-500">LNAPL</th>
+                      <th className="px-6 py-2 text-xs text-gray-500">Edit</th>
+                    </tr>
+                  </thead>
+                  {currentItems.map((volume) => (
+                    <VolumeTable key={volume._id} volumeData={volume} />
+                  ))}
                 </table>
               </div>
+            </div>
           </div>
-        </div>
+
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel=">>"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={3}
+            pageCount={pageCount}
+            previousLabel="<<"
+            renderOnZeroPageCount={null}
+            containerClassName="pagination"
+            pageLinkClassName="page-num"
+            previousLinkClassName="page-num link"
+            nextLinkClassName="page-num link"
+            activeLinkClassName="active"
+          />
+        </>
       )}
     </div>
   );
--- client/src/components/VolumeTable.jsx
+++ client/src/components/VolumeTable.jsx
@@ -1,34 +1,44 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import {date, time} from "../utils/time-date"
+import { date, time } from "../utils/time-date";
 
-const VolumeTable = ({volumeData, tankData}) => {
+const VolumeTable = ({ volumeData }) => {
+
   return (
     <>
-                <tbody className="bg-white divide-y divide-gray-300">
-                  <tr className="whitespace-nowrap">
-                    <td className="px-6 py-4">
-                      <div className="text-sm text-gray-900">{date(volumeData.created_at)}</div>
-                    </td>
-                    <td className="px-6 py-4">
-                      <div className="text-sm text-gray-500">{time(volumeData.created_at)}</div>
-                    </td>
-                    <td className="px-6 py-4 text-sm text-gray-500">{volumeData.totalVol}</td>
-                    <td className="px-6 py-4 text-sm text-gray-500">{volumeData.waterVol}</td>
-                    <td className="px-6 py-4 text-sm text-gray-500">{volumeData.productVol}</td>
-                    <td className="px-6 py-4">
-                      <Link
-                        to={`/editVolume/${volumeData._id}`}
-                        className="px-4 py-1 text-sm text-blue-600 bg-blue-200 rounded-full"
-                      >
-                        Edit
-                      </Link>
-                    </td>
-                  </tr>
-                </tbody>
-              
-              </>
-  )
-}
+      <tbody className="bg-white divide-y divide-gray-300">
+        <tr className="whitespace-nowrap">
+          <td className="px-6 py-4">
+            <div className="text-sm text-gray-900">
+              {date(volumeData.created_at)}
+            </div>
+          </td>
+          <td className="px-6 py-4">
+            <div className="text-sm text-gray-500">
+              {time(volumeData.created_at)}
+            </div>
+          </td>
+          <td className="px-6 py-4 text-sm text-gray-500">
+            {volumeData.totalVol}
+          </td>
+          <td className="px-6 py-4 text-sm text-gray-500">
+            {volumeData.waterVol}
+          </td>
+          <td className="px-6 py-4 text-sm text-gray-500">
+            {volumeData.productVol}
+          </td>
+          <td className="px-6 py-4">
+            <Link
+              to={`/editVolume/${volumeData._id}`}
+              className="px-4 py-1 text-sm text-blue-600 bg-blue-200 rounded-full"
+            >
+              Edit
+            </Link>
+          </td>
+        </tr>
+      </tbody>
+    </>
+  );
+};
 
-export default VolumeTable
+export default VolumeTable;
