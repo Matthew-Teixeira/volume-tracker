7e0269def3180c3ad2a8de6bb52a71d7a4b08f65
ApolloSolo <matt1teixeira@gmail.com>
2022-07-22 18:52:20 -0400

    Updated server route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const db = require("./config/connection");
 const cors = require("cors");
+const path = require("path")
 //const { errorHandler } = require("./middleware/errorMiddleware");
 
 app.use(express.json());
@@ -11,11 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cors())
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 app.use(require("./routes"));
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, '../client/build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"));
+});
 
 db.once("open", () => {
     app.listen(PORT, () => {
