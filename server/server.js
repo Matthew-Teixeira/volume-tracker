16ab93507c047ef29db2dbfb5cd549a5244bd00b
ApolloSolo <matt1teixeira@gmail.com>
2022-07-22 19:20:35 -0400

    Server updata

--- server/server.js
+++ server/server.js
@@ -4,25 +4,29 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const db = require("./config/connection");
 const cors = require("cors");
-const path = require("path")
+const path = require("path");
 //const { errorHandler } = require("./middleware/errorMiddleware");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(cors())
+app.use(cors());
 
 app.use(require("./routes"));
 
 // Serve frontend
-app.use(express.static(path.join(__dirname, '../client/build')))
+if ((process.env.NODE_ENV = "production")) {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    );
+  });
+}
 
 db.once("open", () => {
-    app.listen(PORT, () => {
-      console.log("Listning at port: " + PORT);
-    });
+  app.listen(PORT, () => {
+    console.log("Listning at port: " + PORT);
   });
+});
