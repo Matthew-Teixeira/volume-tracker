82e3752f0ef2906a9daa9e054edad13d32300909
ApolloSolo <matt1teixeira@gmail.com>
2022-07-22 19:00:44 -0400

    Updated server route

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,8 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-    res.status(404).send('<h1>😝 404 Error!</h1>');
-  });
+// router.use((req, res) => {
+//     res.status(404).send('<h1>😝 404 Error!</h1>');
+//   });
 
 module.exports = router
