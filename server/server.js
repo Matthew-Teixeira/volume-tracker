1d3a22d2850ac2d3e8a00af4a10b03751e85c6a9
ApolloSolo <matt1teixeira@gmail.com>
2022-07-22 20:07:49 -0400

    Removed process condition

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(require("./routes"));
 
 // Serve frontend
-if ((process.env.NODE_ENV = "production")) {
+//if ((process.env.NODE_ENV = "production")) {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
@@ -23,7 +23,7 @@ if ((process.env.NODE_ENV = "production")) {
       path.resolve(__dirname, "../", "client", "build", "index.html")
     );
   });
-}
+//}
 
 db.once("open", () => {
   app.listen(PORT, () => {
